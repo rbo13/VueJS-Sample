32c208bcdeef209e48dd5eaafa97855d15d0badc
Richard Orofeo <chardy.orofeo@yahoo.com.ph>
2018-05-16 21:28:22 +0800

    Improved cart

--- public/script.js
+++ public/script.js
@@ -1,19 +1,20 @@
 new Vue({
-
 	el: '#app',
 	data: {
 		total: 0,
 		items: [
-			{ title: 'Item 1' },
-			{ title: 'Item 2' },
-			{ title: 'Item 3' },
-			{ title: 'Item 4' },
-			{ title: 'Item 5' }
-		]
+			{ title: 'Item 1', price: 100 },
+			{ title: 'Item 2', price: 2 },
+			{ title: 'Item 3', price: 3 },
+			{ title: 'Item 4', price: 4 },
+			{ title: 'Item 5', price: 22.30 }
+		],
+		cart: []
 	},
 	methods: {
-		addItem: function() {
-			this.total += 1
+		addItem: function(index) {
+			this.total += this.items[index].price;
+			this.cart.push(this.items[index]);
 		}
 	}
 })
