b871ce232c85a25d970c3e4e29c3bcecdb4ef241
Richard Orofeo <chardy.orofeo@yahoo.com.ph>
2018-05-16 22:49:59 +0800

    Fixed adding of items in cart

--- public/script.js
+++ public/script.js
@@ -1,20 +1,62 @@
+var PRICE = 9.99
 new Vue({
 	el: '#app',
 	data: {
 		total: 0,
 		items: [
-			{ title: 'Item 1', price: 100 },
-			{ title: 'Item 2', price: 2 },
-			{ title: 'Item 3', price: 3 },
-			{ title: 'Item 4', price: 4 },
-			{ title: 'Item 5', price: 22.30 }
+			{ id: 1, title: 'Item 1' },
+			{ id: 2, title: 'Item 2' },
+			{ id: 3, title: 'Item 3' },
+			{ id: 4, title: 'Item 4' },
+			{ id: 5, title: 'Item 5' }
 		],
 		cart: []
 	},
 	methods: {
 		addItem: function(index) {
-			this.total += this.items[index].price;
-			this.cart.push(this.items[index]);
+			// this.total += this.items[index].price;
+			this.total += PRICE; 
+			var item = this.items[index];
+			var found = false;
+			for (var i = 0; i < this.cart.length; i++) {
+				if (this.cart[i].id === item.id) {
+					found = true;
+					this.cart[i].qty++;
+					break;
+				}
+			}
+			if (!found) {
+				this.cart.push({
+					id: item.id,
+					title: item.title,
+					qty: 1,
+					price: PRICE
+				});
+			}
+		},
+		inc: function(item) {
+			item.qty++;
+			this.total += PRICE;
+		},
+		dec: function(item) {
+			item.qty--;
+			this.total -= PRICE;
+			if (item.qty <= 0) {
+				for (var i = 0; i < this.cart.length; i++) {
+					if (this.cart[i].id === item.id) {
+						this.cart.splice(i, 1);
+						break;
+					}
+				}
+			}
 		}
+	},
+
+	filters: {
+		currency: function(price) {
+			return '$'.concat(price.toFixed(2));
+		}
+
 	}
+
 })
